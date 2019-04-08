9dab3f8900ef45c881f7a224fff9ace3f1c9069b
Luis A. Celis <luis.celis.peralta@gmail.com>
2019-04-08 16:34:17 -0600

    Migracion de la bd

---
+++ database/migrations/2019_04_07_224425_create_reservas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre_cliente', 50);
+            $table->string('apellido_cliente', 50);
+            $table->dateTime('fecha_nacimiento');
+            $table->dateTime('inicio_reserva');
+            $table->dateTime('fin_reserva');
+            $table->string('habitacion');
+            $table->float('costo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservas');
+    }
+}
