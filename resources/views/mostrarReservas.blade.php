459401597faf045bff39f3bab4355dae000fb855
Luis A. Celis <luis.celis.peralta@gmail.com>
2019-04-11 03:19:55 -0600

    omitir

--- resources/views/mostrarReservas.blade.php
+++ resources/views/mostrarReservas.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"              integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="{{asset('js/angular.js')}}" type="text/javascript"></script>
+    <script src="{{asset('js/moment.min.js')}}" type="text/javascript"></script>
+
     <title>Hotel: Marcel</title>
 </head>
 <body ng-controller="ctrl">
@@ -38,7 +40,7 @@
                     <td>{{ $reserva->status }}</td>                    
                     <td class="text-success">$ {{ $reserva->costo }}</td>                    
                     <td><a href="{{ route('reserva.edit', $reserva -> id) }}"><button class="btn btn-warning" id="btnEditarReserva">Editar</button> </a></td>
-                    <td><a><button class="btn btn-danger" id="btnCancelar" ng-click="mandarMantenimiento(  {{ $reserva }} )">Cancelar</button></a></td>
+                    <td><a><button class="btn btn-danger" id="btnCancelar" ng-click="bajaReserva( {{ $reserva }} )">Cancelar</button></a></td>
                 </tr>
                 @endforeach
             </tbody>
@@ -55,32 +57,14 @@
            $scope.minimaCuartos = 2;
            $scope.mostrarBaja = false; //apartado para dar de baja
             
-           $scope.mandarMantenimiento = function( objetoRecibido ) {
-               let cantidadBajar = prompt ( '¿Cuantos cuartos desea dar de baja?' );
-               if(!isNaN(cantidadBajar) && cantidadBajar != null && cantidadBajar != ""){
-                   prompt( 'Describe el motivo: ');
-                   let confirmacion = confirm( '¿Esta seguro de quitar ' + cantidadBajar + ' cuartos?');
-                   console.log( cantidadBajar );
-                   if( confirmacion ) {
-                       objetoRecibido.cantidad_cuartos = objetoRecibido.cantidad_cuartos - cantidadBajar;
-                       if ( objetoRecibido.cantidad_cuartos < $scope.minimaCuartos ){
-                           alert ( 'No se puede quitar todos los cuartos' );
-                           return;
-                       }else {
-                            $http.post('/updateCuartos/' + objetoRecibido.id, objetoRecibido).then(
-                            function(response){
-                                alert("¡Registro modificado con éxito!");
-                                location.reload();
-                            },function(errorResponse){
-                                alert("Ha ocurrido un error al modificar");
-                            });
-                       }
-                   }  
-  	           }else{
-                   alert('Favor de ingresar un número válido');
-  	           }
-               if ( !cantidadBajar ){
-                   return;
+           $scope.bajaReserva = function( objetoReserva ) {
+               console.log( objetoReserva );
+               let inicioReserva = moment(objetoReserva.inicio_reserva);
+               let fechaAcutal = moment();
+
+               if( inicioReserva >= fechaAcutal ){
+                   console.log(inicioReserva);
+
                }
            }
         });
